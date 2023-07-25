a58d69fcc5254fbd016a3349592513c4aaa586e5
rtpro5626 <136262363+rtpro5626@users.noreply.github.com>
2023-07-25 13:39:23 -0700

    Add files via upload

---
+++ tinke_page.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDt7KhAmwYMPeMLgpsYRtRiv63Nmr5tG3w",
+    authDomain: "tinke-d2bfc.firebaseapp.com",
+    databaseURL: "https://tinke-d2bfc-default-rtdb.firebaseio.com",
+    projectId: "tinke-d2bfc",
+    storageBucket: "tinke-d2bfc.appspot.com",
+    messagingSenderId: "106987243987",
+    appId: "1:106987243987:web:4a9f4a62a2a9082eea4726"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
